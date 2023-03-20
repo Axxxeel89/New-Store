39419ce4c2ab73e9091caaf83d0898f9720383f7
Alex Rios <john.alexander.rios@gmail.com>
2023-03-20 16:34:42 -0500

    Vamos a empezar a implementar Material en nuestros formularios

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,5 +1,6 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
-import { FormControl} from '@angular/forms'; //-> Nos permitira trabajar con formularios reactivos.
+import { FormControl, Validators, FormGroup, FormBuilder} from '@angular/forms'; //-> Nos permitira trabajar con formularios reactivos.
 
 @Component({
   selector: 'app-basic-form',
@@ -8,33 +9,139 @@ import { FormControl} from '@angular/forms'; //-> Nos permitira trabajar con for
 })
 export class BasicFormComponent implements OnInit {
 
-  //Lo primero que debemos hacer es instanciar la clase FormControl
-  nameField = new FormControl('Soy un control'); //-> Dentro de los parentesis podemos inicializar esta propiedad con un valor
-  emailField = new FormControl();
-  phoneField = new FormControl();
-  colorField = new FormControl();
-  dateField = new FormControl();
-  ageField = new FormControl();
-  textField = new FormControl();
-  imgField = new FormControl();
+  form: FormGroup;
 
-  constructor() { }
+  //FormBuilder es un servicio por lo tanto debemos inyectarlo
+  constructor(
+    private formBuilder:FormBuilder
+  ) {
+    this.buildForm(); //-> Despues de inyectado hacemos el llamado desde esta linea.
+   }
 
   ngOnInit(): void {
-    //Usaremos el observable valueChanges
-    this.nameField.valueChanges //-> Podemos indicar que vamos a estar validando en todo momento los cambios
-    .subscribe(value => {
-      console.log(value);
-    })
-
-    this.colorField.valueChanges //-> Podemos indicar que vamos a estar validando en todo momento los cambios
-    .subscribe(value => {
-      console.log(value);
-    })
+    // this.form.valueChanges
+    // .subscribe(values => {
+    //   console.log(values)
+    // })
   }
 
-  getValue(){
-    console.log(this.nameField.value); //--> Con esto obtengo el valor del formControl
+  saveForm(event: Event){
+    if(this.form.valid)
+    {
+      console.log(this.form.value)
+      alert(stringify(this.form.value))
+    }else{
+      this.form.markAllAsTouched();  //->Lo que hace este metodo es activar el touched
+      alert("hubo un error")
+    }
+
   }
+
+  //Creamos un metodo prvado para poder utilizar el FormBuilder
+  private buildForm(){
+    this.form = this.formBuilder.group({
+      fullName: this.formBuilder.group({ //-> De esta manera anidamos un FormGroup
+        name: ['', [Validators.required, Validators.maxLength(15), Validators.pattern(/[a-zA-Z ]+$/)]],
+        lastName: ['', [Validators.required, Validators.maxLength(15), Validators.pattern(/[a-zA-Z ]+$/)]]
+      }),
+      email: ['', [Validators. required, Validators.email]],
+      phone: ['', Validators.required],
+      color: ['#000000'],
+      date: [''],
+      age: [18, [Validators. required, Validators.min(18), Validators.max(80)]],
+      text:[''],
+      img: [''],
+      category: ['category-3'],
+      tag: [''],
+      agreed: [false,  Validators.requiredTrue], //-->Con esto validamos que sea true
+      gender: [''],
+
+    });
+  }
+
+  //Agregamos los metodos get para key del formGroup esto con el fin de obtener la referencia de cada uno y evitarnos repetir codigo
+  get nameField(){
+    return this.form.get('fullName.name'); //--> Para acceder a la propiedad name o formGrouop anidado
+  }
+
+  get lastField(){
+    return this.form.get('fullName.lastName'); //--> Para acceder a la propiedad name o formGrouop anidado
+  }
+
+  get emailField(){
+    return this.form.get('email')
+  }
+
+  get phoneField(){
+    return this.form.get('phone')
+  }
+
+  get colorField(){
+    return this.form.get('color')
+  }
+
+  get dateField(){
+    return this.form.get('date')
+  }
+
+  get ageField(){
+    return this.form.get('age')
+  }
+
+  get textField(){
+    return this.form.get('text')
+  }
+
+  get imgField(){
+    return this.form.get('img')
+  }
+
+  get categoryField(){
+    return this.form.get('category')
+  }
+
+  get tagField(){
+    return this.form.get('tag')
+  }
+
+  get agreedField(){
+    return this.form.get('agreed')
+  }
+
+  get genderField(){
+    return this.form.get('gender')
+  }
+
+  get isNameFieldValid() {
+    return this.nameField.touched && this.nameField.valid
+  }
+
+  get isNameFieldInValid() {
+    return this.nameField.touched && this.nameField.invalid
+  }
+
+  get isLastNameFieldInValid() {
+    return this.lastField.touched && this.lastField.invalid
+  }
+
+  get isPhoneFieldValid() {
+    return this.phoneField.touched && this.phoneField.valid
+  }
+
+  get isPhoneFieldInValid() {
+    return this.phoneField.touched && this.phoneField.invalid
+  }
+
+  get isEmailInValid() {
+    return this.emailField.touched && this.emailField.invalid
+  }
+
+  get IsAgeInValid(){
+    return this.ageField.touched && this.ageField.invalid
+  }
+  get isAgreedFieldInValid(){
+    return this.agreedField.touched && this.agreedField.invalid
+  }
+
 
 }
